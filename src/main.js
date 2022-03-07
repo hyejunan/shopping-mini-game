24b14480c0e962c2b90a87ab0b3510e9ed0fc29d
hyejun-an <hyejunan.canada@gmail.com>
2022-03-06 20:53:22 -0500

    Update main.js

---
+++ src/main.js
@@ -0,0 +1,15 @@
+'use strict'
+
+
+
+const header = document.querySelector('.header');
+const filter = document.querySelector('.filter');
+const list = document.querySelector('.list');
+let colorArr = ['blue', 'yellow', 'pink'];
+let typeArr = ['tshirt', 'pant', 'skirt'];
+let genderArr = ['female', 'male'];
+let sizeArr = ['small size', 'medium size', 'large size'];
+
+let arr = [];
+
+
