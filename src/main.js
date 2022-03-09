12a9e25c0060c08efb12fe3da01d20fcf8d452d7
hyejun-an <hyejunan.canada@gmail.com>
2022-03-08 22:58:13 -0500

    create item & push to arr

--- src/main.js
+++ src/main.js
@@ -34,9 +34,20 @@ function start() {
     const item1 = new Item('female', 'large', 'tshirt', 'blue', 'img/blue_t.png');
     const item2 = new Item('male', 'small', 'pant', 'pink', 'img/pink_p.png');
     const item3 = new Item('male', 'medium', 'skirt', 'yellow', 'img/yellow_s.png');
-    addListItem(item1);
-    addListItem(item2);
-    addListItem(item3);
+    const item4 = new Item('female', 'medium', 'pant', 'blue', 'img/blue_p.png');
+    const item5 = new Item('male', 'small', 'skirt', 'pink', 'img/pink_s.png');
+    const item6 = new Item('male', 'large', 'tshirt', 'yellow', 'img/yellow_t.png');
+    const item7 = new Item('male', 'medium', 'skirt', 'blue', 'img/blue_s.png');
+    arr.push(item1);
+    arr.push(item2);
+    arr.push(item3);
+    arr.push(item4);
+    arr.push(item5);
+    arr.push(item6);
+    arr.push(item7);
+    for(let i =0;i<arr.length;i++) {
+        addListItem(arr[i]);
+    }
 }
 
 start();
