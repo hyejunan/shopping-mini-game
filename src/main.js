77fb70c7be34f38abfcf1eccb00d4efad594c930
hyejun-an <hyejunan.canada@gmail.com>
2022-03-08 22:51:12 -0500

    Create Item class& add to list

--- src/main.js
+++ src/main.js
@@ -1,22 +1,45 @@
 'use strict'
 
+const LIST_COUNT = 10;
+
 const header = document.querySelector('.header');
 const filter = document.querySelector('.filter');
 const list = document.querySelector('.list');
-let colorArr = ['blue', 'yellow', 'pink'];
-let typeArr = ['tshirt', 'pant', 'skirt'];
-let genderArr = ['female', 'male'];
-let sizeArr = ['small size', 'medium size', 'large size'];
 
 let arr = [];
 
-function filterList(e) {
-    return arr.filter(i => i===e);
+class Item {
+    constructor(gender, size, type, color, imgPath) {
+        this.gender = gender;
+        this.size = size;
+        this.type = type;
+        this.color = color;
+        this.path = imgPath;
+    }
 }
 
-filter.addEventListener('click', (e) => {
-    
-})
+function addListItem(i) {
+    const li = document.createElement('li');
+    const img = document.createElement('img');
+    const text = document.createTextNode(`${i.gender}, ${i.size} size`);
+    li.setAttribute('class', i.type);
+    li.setAttribute('class', i.color);
+    img.setAttribute('src', i.path);
+    li.appendChild(img);
+    li.appendChild(text);
+    list.appendChild(li);
+}
+
+function start() {
+    const item1 = new Item('female', 'large', 'tshirt', 'blue', 'img/blue_t.png');
+    const item2 = new Item('male', 'small', 'pant', 'pink', 'img/pink_p.png');
+    const item3 = new Item('male', 'medium', 'skirt', 'yellow', 'img/yellow_s.png');
+    addListItem(item1);
+    addListItem(item2);
+    addListItem(item3);
+}
+
+start();
 
 
 
