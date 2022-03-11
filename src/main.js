9060da366e8e68f9ab67680120129e2366d50fff
hyejun-an <hyejunan.canada@gmail.com>
2022-03-11 07:11:49 -0500

    Update data.json

--- src/main.js
+++ src/main.js
@@ -1,2 +1,16 @@
 'use strict'
 
+
+
+// Fetch the items from the JSON file
+function loadItems() {
+    return fetch('data/data.json').then(response => console.log(response));
+}
+
+// main
+loadItems()
+.then(items => {
+    // displayItems(items);
+    // setEventListners(items);
+})
+.catch(console.log)
